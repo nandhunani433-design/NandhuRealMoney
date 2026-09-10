760f022c28946011e4311ebbb1cc26aa1f708cf2
nandhunani433-design <nandhunani433@gmail.com>
2026-09-10 16:19:57 +0530

    Set Java compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,12 @@ plugins {
 android {
     namespace = "com.nandhurealmoney.app"
     compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     defaultConfig {
         applicationId = "com.nandhurealmoney.app"
         minSdk = 24
