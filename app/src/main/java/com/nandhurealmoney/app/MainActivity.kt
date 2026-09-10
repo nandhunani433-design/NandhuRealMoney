d545f950f0a5aa4898b3e5332c03a1f9774f0206
nandhunani433-design <nandhunani433@gmail.com>
2026-09-10 19:01:57 +0530

    Add vertical scroll functionality to MainActivity

--- app/src/main/java/com/nandhurealmoney/app/MainActivity.kt
+++ app/src/main/java/com/nandhurealmoney/app/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
